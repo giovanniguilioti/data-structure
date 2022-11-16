829a4d13125303b6eb38a28abc9a49281f4ce23a
giovanniguilioti <ngiovanni.guilioti@gmail.com>
2022-11-16 19:24:31 -0300

    header linked list insert

--- list/header_linkedlist.c
+++ list/header_linkedlist.c
@@ -65,3 +65,88 @@ int ElementAt(struct node* head, int index)
     
     return temp->data;
 }
+
+int InsertBegin(struct node** head, int value)
+{
+    struct node* new = malloc(sizeof(struct node));
+    if(new == NULL)
+        return 0;
+
+    if(*head == NULL)
+    {
+        *head = malloc(sizeof(struct node));
+        (*head)->next = NULL;
+    }
+    new->data = value;
+    new->next = (*head)->next;
+    (*head)->next = new;
+
+    return 1;
+}
+
+int InsertAt(struct node** head, int index, int value)
+{
+    if(index < 0 || index > ListSize(*head))
+        return 0;
+
+    struct node* new = malloc(sizeof(struct node));
+    if(new == NULL)
+        return 0;
+
+    new->data = value;
+    new->next = NULL;
+
+    if(index == 0)
+    {
+        if(*head == NULL)
+        {
+            *head = malloc(sizeof(struct node));
+            (*head)->next = NULL;
+        }
+
+        new->next = (*head)->next;
+        (*head)->next = new;
+        return 1;
+    }
+
+    struct node* temp = (*head)->next;
+    int i = 1;
+    while(i < index)
+    {
+        temp = temp->next;
+        ++i;
+    }
+
+    new->next = temp->next;
+    temp->next = new;
+    return 1;
+}
+
+int InsertEnd(struct node** head, int value)
+{
+    struct node* new = malloc(sizeof(struct node));
+    if(new == NULL)
+        return 0;
+    
+    new->data = value;
+    new->next = NULL;
+
+    if(*head == NULL)
+    {
+        *head = malloc(sizeof(struct node));
+        (*head)->next = new;
+        return 1;
+    }
+    else if((*head)->next == NULL)
+    {
+        (*head)->next = new;
+        return 1;
+    }
+
+    struct node* temp = (*head)->next;
+    while(temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = new;
+    return 1;
+}
