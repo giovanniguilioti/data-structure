20dd3faa2384ccec9d0bf05bc68495ec8b0931c5
giovanniguilioti <ngiovanni.guilioti@gmail.com>
2022-11-17 20:06:45 -0300

    binary tree traversal

--- tree/binarytree.c
+++ tree/binarytree.c
@@ -8,10 +8,40 @@ int IsEmpty(struct node* root)
     return 0;
 }
 
-int Nodes(struct node* root)
+int Nodes(struct node* node)
 {
     if(root == NULL)
         return 0;
     
     return 1 + Nodes(root->left) + Nodes(root->right);
+}
+
+void InOrder(struct node* node)
+{
+    if(node == NULL)
+        return;
+
+    InOrder(node->left);
+    //process the node
+    InOrder(node->right);
+}
+
+void PreOrder(struct node* node)
+{
+    if(node == NULL)
+        return;
+
+    //process the node
+    PreOrder(node->left);
+    PreOrder(node->right);
+}
+
+void PostOrder(struct node* node)
+{
+    if(node == NULL)
+        return;
+
+    PostOrder(node->left);
+    PostOrder(node->right);
+    //process the node
 }
--- tree/binarytree.h
+++ tree/binarytree.h
@@ -14,4 +14,10 @@ int IsEmpty(struct node* root);
 
 int Nodes(struct node* root);
 
+void InOrder(struct node* root);
+
+void PreOrder(struct node* root);
+
+void PostOrder(struct node* root);
+
 #endif
